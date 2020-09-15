23e2ca9e8a4f7f5c4d9ccb36e549cee80c62a444
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2020-09-15 12:13:23 +0200

    finished document/document-dirty-dialog

---
+++ src/components/Document/DocumentDirtyDialog/index.js
@@ -0,0 +1,5 @@
+import DocumentDirtyDialog from './view/DocumentDirtyDialog';
+
+export default DocumentDirtyDialog;
+
+export * from './redux/isDocumentDirty';
---
+++ src/components/Document/DocumentDirtyDialog/redux/isDocumentDirty.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const isDocumentDirtySlice = createSlice({
+  name: 'isDocumentDirty',
+  initialState: false,
+  reducers: {
+    setIsDocumentDirty: (state, action) => action.payload,
+  },
+});
+
+export const { setIsDocumentDirty } = isDocumentDirtySlice.actions;
+
+export default isDocumentDirtySlice.reducer;
+
+export const getIsDocumentDirty = (state) => state.isDocumentDirty;
